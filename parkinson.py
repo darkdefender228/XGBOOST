da020fba270ac5345953ef3dcf75ac07b8bb2fb0
Roman Ilechko <rooooman69@gmail.com>
2018-04-06 22:35:24 +0300

    Add files via upload

--- parkinson.py
+++ parkinson.py
@@ -5,27 +5,48 @@ Created on Thu Apr  5 23:58:05 2018
 
 @author: romanilechko
 """
-
-import numpy as np
+# importing libs
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cross_validation import train_test_split
 from xgboost.sklearn import XGBClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
 
+N_TIMES = 20
+
+# reading data
 df = pd.read_csv("parkinsons.csv")
 df = df.iloc[:, 1:]
 
+# extracting useful feature
 features = df.loc[:, df.columns != 'status'].values
 labels = df.loc[:, 'status'].values
 
+# scaling feature
 scale = MinMaxScaler((-1, 1))
 X = scale.fit_transform(features)
 
-X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.1)
+# calculating mean accuracy
+def model(times, X, labels, n_components=None):
+    acc = []
+    
+    for t in range(times):        
+        if n_components != None:
+            pca = PCA(n_components=n_components)
+            X = pca.fit_transform(X)
+            
+        X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.14)
 
-model = XGBClassifier()
-model.fit(X_train, y_train)
+        
+        model = XGBClassifier()
+        model.fit(X_train, y_train)
+    
+        y_train = [round(y) for y in model.predict(X_test)]
+        acc.append(accuracy_score(y_test, y_train))
+        
+        print("acc ", acc[-1])
+    
+    return sum(acc)/times
 
-y_train = [round(yhat) for yhat in model.predict(X_test)]
-print(accuracy_score(y_test, y_train))
+print("mean accuracy ",model(N_TIMES, X, labels))
